bffd6aa1b0961c6214171d486260b13d700c7bd5
Nacho Eigu <ignacio.eiguren@gmail.com>
2022-04-13 15:55:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
 from model import Data_Extractor, Data_Transformation
-
-app = FastAPI()
-
+import time
+#First we make requests in the catalog
 data_extraction = Data_Extractor()
-data_extraction.main()
-data_transformation = Data_Transformation(data_extraction)
-data_transformation.cleaning_html()
+data_extraction.main(mode = True)
 
-@app.get('/get-books')
-def get_books():
-    return {"Name":data_transformation.items}
+#Then we parse the data so we obtain the URL of each book
+data_transformation = Data_Transformation(data_extraction)
+data_transformation.cleaning_html(mode = True)
+urls = data_transformation.return_urls() #List of urls to requests again but in this case its for each book
+
+
+#We make a request for each book url
+books_extraction = Data_Extractor(urls)
+books_extraction.main(mode = False)
+
+#Then we parse the data so we obtain the details of each book
+data_transformation = Data_Transformation(books_extraction)
+data_transformation.cleaning_html(mode = False)
+data_transformation.to_pandas()
